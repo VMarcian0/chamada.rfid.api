1bdac831f2f4fd338ec89e5b37b3961b8b5a3380
vmarciano <vmarciano@gmalato.com.br>
2021-11-06 17:07:34 -0300

    verify class room on invoke embedded system

--- src/services/embedded/embedded.hooks.ts
+++ src/services/embedded/embedded.hooks.ts
@@ -1,10 +1,12 @@
 import * as authentication from '@feathersjs/authentication';
-import { Unprocessable } from '@feathersjs/errors';
+import { NotFound, Unprocessable } from '@feathersjs/errors';
 import { HookContext } from '@feathersjs/feathers';
 import axios, { AxiosResponse } from "axios";
+import app from '../../app';
+import { ClassroomType } from '../../types';
 // Don't remove this comment. It's needed to format import lines nicely.
 
-const url = 'chamadarfid.ddns.net:9090/'
+const url = 'http://chamadarfid.ddns.net:9090/'
 
 const { authenticate } = authentication.hooks;
 /**
@@ -19,13 +21,19 @@ const methodSwitch = async (context:HookContext) => {
    * Starts the working of the module
    */
   if (context.data.method == "<INIT>"){
-    
-    const classroom = context.data.classroom as string;
+    if (!context.data?.classroom) throw new Unprocessable("Missing classroom id");
+    const classroomId = context.data?.classroom as string;
+    const classroom = await app.services.classrooms._get(classroomId as string) as ClassroomType;
+    if (!classroom?._id) throw new NotFound("Classroom Not Found",{classroom:classroomId});
     const payload = {
       token,
       classroom
     }
-    let {data} = await axios.post(url+'init_attendance', JSON.stringify(payload))
+    const headers: { [key: string]: string } = {
+      "Content-Type": "application/json"
+    };
+    let endpoint = url+'init_attendance' as string
+    let {data} = await axios.post(endpoint , JSON.stringify(payload),{headers}) as AxiosResponse<String>
     if (data == 'Chamada Iniciada'){
       context.result = "Success!";
       return context;
